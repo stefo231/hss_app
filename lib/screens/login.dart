f456da3c7b175001700addcf39e3f3a4f99de324
Stefan Zdravkov <szdravkov93@gmail.com>
2020-10-07 13:29:44 +0200

    login btn

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,17 +9,124 @@ class _LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          //! Initial template code.
-          FlatButton(onPressed: () {}, child: Text('circle btn back')),
-          Text('Email'),
-          TextField(),
-          Text('Password'),
-          TextField(),
-          FlatButton(onPressed: () {}, child: Text('Login'))
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/group_372.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //! Initial template code.
+            FlatButton(
+                onPressed: () {
+                  //Navigator.pop(context);
+                },
+                child: Icon(Icons.arrow_back_ios)),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 40.0),
+                  child: Text(
+                    'Email',
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+                    onChanged: (value) {},
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'john.doe@email.com',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 40.0),
+                  child: Text(
+                    'Password',
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+                    onChanged: (value) {},
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: '123456789',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            GestureDetector(
+              onTap: () {
+                print('Hello!');
+              },
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                //color: Colors.grey[300],
+                margin: EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                ),
+                padding: EdgeInsets.only(
+                  top: 10.0,
+                  bottom: 10.0,
+                ),
+                decoration: BoxDecoration(
+                    color: Color(0xFFDFE6EE),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(15.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[500],
+                        blurRadius: 10.0,
+                      ),
+                    ]),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
