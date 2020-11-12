e6c11ae9160422d5d202e0373e925f358107d03f
Stefan Zdravkov <szdravkov93@gmail.com>
2020-11-12 01:05:21 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: HomeScreen.id,
+      initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
         LogInEmailScreen.id: (context) => LogInEmailScreen(),
